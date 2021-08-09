30c191922a8dd20c2bb9d509d6a4644b4bd1944d
guilherme.leles <guilherme.leles@concert.com.br>
2021-08-09 20:28:19 -0300

    change folder name

---
+++ challenge/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/login/login.component';
+import { ClientComponent } from './client/client.component';
+import { ProductComponent } from './product/product.component';
+import { AuthGuard } from './services/auth.guard';
+import { ThemeComponent } from './theme/theme.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'client',
+    loadChildren: () =>
+      import('./client/client.module').then((m) => m.ClientModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('./product/product.module').then((m) => m.ProductModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'theme',
+    component: ThemeComponent,
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- challenge/src/app/app.module.ts
+++ challenge/src/app/app.module.ts
@@ -6,45 +6,44 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { GlobalErrorHandler } from './services/global-error-handler.service';
 import { RequestInterceptor } from './interceptor/request-interceptor.interceptor';
 import { ProductModule } from './product/product.module';
 import { SharedModule } from './shared/material/shared.module';
-import { ClientModule } from './client/client.module'
-
+import { ClientModule } from './client/client.module';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-  ],
+  declarations: [AppComponent, LoginComponent, ThemeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
     MatNativeDateModule,
     ProductModule,
     SharedModule,
-    ClientModule
+    ClientModule,
   ],
-  providers: [{provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
-    { 
-    // processes all errors
-    provide: ErrorHandler, 
-    useClass: GlobalErrorHandler,
-    // multi: true 
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    {
+      // processes all errors
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler,
+      // multi: true
     },
-    {   
+    {
       provide: HTTP_INTERCEPTORS,
       useClass: RequestInterceptor,
-      multi: true
+      multi: true,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
